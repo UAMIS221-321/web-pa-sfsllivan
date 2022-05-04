580e8f5f7364ad6452ac51673e48003360ada4cd
Sam Sullivan <sfsullivan1@crimson.ua.edu>
2022-05-04 11:30:10 -0500

    hahah

---
+++ WEB PA ACTUAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WEB_PA_ACTUAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
